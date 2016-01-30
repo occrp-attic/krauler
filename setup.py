18ee981e1ae9beb9243ef20b432f431f78ee0427
Friedrich Lindenberg <friedrich@pudo.org>
2016-01-30 18:25:39 +0100

    basic python package, import aleph code

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='krauler',
+    version='0.1',
+    description="A minimalistic, recursive web crawling library for Python.",
+    long_description="",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+    keywords='',
+    author='Friedrich Lindenberg',
+    author_email='friedrich@pudo.org',
+    url='http://pudo.org',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=[],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'six',
+        'requests >= 2.5',
+        'lxml >= 3',
+        'PyYAML >= 3.10',
+        'urlnorm'
+    ],
+    entry_points={},
+    tests_require=[]
+)
