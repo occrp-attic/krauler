f8c0f3b4006128138c800b4279d948a4b373620a
Friedrich Lindenberg <friedrich@pudo.org>
2016-05-30 11:29:50 +0200

    release 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='krauler',
-    version='0.2.0',
+    version='0.2.1',
     description="A minimalistic, recursive web crawling library for Python.",
     long_description="",
     classifiers=[
